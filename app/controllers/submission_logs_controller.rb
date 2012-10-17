89541e3fcb7edd4c20c1787b7177060d7f6bc245
Jesse Prabawa <jprabawa@comp.nus.edu.sg>
2012-10-17 20:19:23 +0800

    Add role authentication for submission log

--- app/controllers/submission_logs_controller.rb
+++ app/controllers/submission_logs_controller.rb
@@ -1,8 +1,16 @@
 class SubmissionLogsController < ApplicationController
+  before_filter { |controller|
+    if params[:assignment_id]
+      @assignment = Assignment.find(params["assignment_id"])
+      @course = @assignment.course
+      controller.send :authenticate_actions_for_role, UserCourseMembership::ROLE_STUDENT,
+                                                      course: @course,
+                                                      only: [ ]
+    end
+  }
+
   # GET /assignments/1/submissions/1/log
   def index
-    @assignment = Assignment.find(params["assignment_id"])
-    @course = @assignment.course
     @submission = Submission.find(params["submission_id"])
     @submission_logs = @submission.logs
   end
