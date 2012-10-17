5cf8db2489af925a52dcba8bdbd388b85737cad3
Jesse Prabawa <jprabawa@comp.nus.edu.sg>
2012-10-17 19:28:42 +0800

    Clean up courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -31,49 +31,4 @@ class CoursesController < ApplicationController
       }
     end
   end
-
-# # GET /courses/1
-# def show
-#   @course = Course.find(params[:id])
-# end
-
-# # GET /courses/new
-# def new
-#   @course = Course.new
-# end
-
-# # GET /courses/1/edit
-# def edit
-#   @course = Course.find(params[:id])
-# end
-
-# # POST /courses
-# def create
-#   @course = Course.new(params[:course])
-
-#   if @course.save
-#     redirect_to @course, notice: 'Course was successfully created.'
-#   else
-#     render action: "new"
-#   end
-# end
-
-# # PUT /courses/1
-# def update
-#   @course = Course.find(params[:id])
-
-#   if @course.update_attributes(params[:course])
-#     redirect_to @course, notice: 'Course was successfully updated.'
-#   else
-#     render action: "edit"
-#   end
-# end
-
-# # DELETE /courses/1
-# def destroy
-#   @course = Course.find(params[:id])
-#   @course.destroy
-# 
-#   redirect_to courses_url
-# end
 end
