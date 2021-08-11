dece9705029ba61f37ffd6dcd482316f7428dabc
Riyas97 <rockingriyas97@yahoo.com>
2021-08-11 14:53:24 +0800

    add logic for users to use sample files

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -25,6 +25,7 @@ class AssignmentsController < ApplicationController
   APPLICATION_ZIP_MIME_TYPE = "application/zip"
   MULTIPART_X_ZIP_MIME_TYPE = "multipart/x-zip"
   OCTET_STREAM_MIME_TYPE = "application/octet-stream"
+ 
 
   before_action { |controller|
     if params[:course_id]
@@ -35,21 +36,11 @@ class AssignmentsController < ApplicationController
       controller.send :authenticate_actions_for_role, UserCourseMembership::ROLE_STUDENT,
                                                       course: @course,
                                                       only: [ ]
-      controller.send :authenticate_actions_for_role, UserCourseMembership::ROLE_GUEST,
-                                                      course: @course,
-                                                      only: [ :index ]
     end
   }
 
   # GET /courses/1/assignments
   def index
-    
-    # check whether is it a guest user 
-    @user = User.find_by_id(session[:user_id]) 
-    @guest_user = GuestUsersDetail.find_by_user_id(@user.id)
-    # obtain assignment to be shown if is a guest user
-    @assignment_to_be_shown = @guest_user.assignment_id
-    
     @assignments = @course.assignments
     @empty_assignments = @course.empty_assignments
     @processing_assignments = @course.processing_assignments
@@ -96,41 +87,26 @@ class AssignmentsController < ApplicationController
 
     # Process file if @assignment is valid and file was uploaded
     if @assignment.valid?
- 
+     
       # Save assignment to obtain id
       return render action: "new" unless @assignment.save
-
-      isMapEnabled = (params[:assignment]["mapbox"] == "Yes")? true : false;
-
-      # No student submission file was uploaded
-      if params[:assignment]["file"].nil?
-        # Create asssignment but don't process it
-        redirect_to course_assignments_url(@course), notice: 'Assignment was successfully created. Please upload the student submission files and mapping file (if any) to process the assignment'
-      # Student submission file is a valid zip
-      elsif (is_valid_zip?(params[:assignment]["file"].content_type, params[:assignment]["file"].path))
-        # Don't process the file and show error if the mapping was enabled but no mapping file was uploaded
-        if (is_valid_map_or_no_map?(isMapEnabled, params[:assignment]["mapfile"])) 
-          self.start_upload(@assignment, params[:assignment]["file"], isMapEnabled, params[:assignment]["mapfile"])
-          redirect_to course_assignments_url(@course), notice: 'Assignment was successfully created. Please refresh this page after a few minutes to view the similarity results.'
-        # Don't process the file and show error if the mapping was enabled but no mapping file was uploaded
-        elsif (isMapEnabled && params[:assignment]["mapfile"].nil?)
-          @assignment.errors.add(:mapfile, "containing mapped student names need to be uploaded if the 'Upload map file' box is ticked")
-          return render action: "new"
-        # Don't process the file and show error if the mapping was enabled but no mapping file was uploaded
-        else 
-          @assignment.errors.add :mapfile, "containing mapped student names must be a valid csv file"
-          return render action: "new"
+      
+      if !params[:assignment]["file"].nil?
+        if (is_valid_zip?(params[:assignment]["file"].content_type, params[:assignment]["file"].path))
+          self.start_upload(@assignment, params[:assignment]["file"])
+          redirect_to course_assignments_url(@course), notice: 'Assignment was successfully created.'
+        else
+          if params[:assignment]["file"].nil?
+            @assignment.errors.add :file, "is not selected for upload"
+          elsif params[:assignment]["file"].content_type != "application/x-zip-compressed"
+            @assignment.errors.add :file, "for upload must be a zip file"
+          end
+         return render action: "new"
         end
-      # Student submission file is not a valid zip file
       else
-        @assignment.errors.add :file, "containing student submission files must be a valid zip file"
-        if (params[:assignment]["mapfile"].nil? && isMapEnabled)
-          @assignment.errors.add :mapfile, "containing mapped student names need to be uploaded if the 'Upload map file' box is ticked"
-        elsif !(is_valid_map_or_no_map?(isMapEnabled, params[:assignment]["mapfile"])) 
-          @assignment.errors.add :mapfile, "containing mapped student names must be a valid csv file"
-        end
-        return render action: "new"
+        redirect_to course_assignments_url(@course), notice: 'Assignment was successfully created.'
       end
+      
     else
       render action: "new"
     end
@@ -139,41 +115,17 @@ class AssignmentsController < ApplicationController
   # PUT /courses/1/assignments/1
   def update
     @assignment = Assignment.find(params[:id])
-
-    isMapEnabled = (params[:assignment]["mapbox"] == "Yes")? true : false;
-
-    # No student submission file was uploaded
-    if params[:assignment]["file"].nil?
-      @assignment.errors.add :file, "containing student submission files need to be uploaded to process the assignment"
-        if (params[:assignment]["mapfile"].nil? && isMapEnabled)
-          @assignment.errors.add :mapfile, "containing mapped student names need to be uploaded if the 'Upload map file' box is ticked"
-        elsif !(is_valid_map_or_no_map?(isMapEnabled, params[:assignment]["mapfile"])) 
-          @assignment.errors.add :mapfile, "containing mapped student names must be a valid csv file"
-        end
-      return render action: "show"
-    elsif (is_valid_zip?(params[:assignment]["file"].content_type, params[:assignment]["file"].path))
-      # Don't process the file and show error if the mapping was enabled but no mapping file was uploaded
-      if (is_valid_map_or_no_map?(isMapEnabled, params[:assignment]["mapfile"])) 
-        self.start_upload(@assignment, params[:assignment]["file"], isMapEnabled, params[:assignment]["mapfile"])
-        redirect_to course_assignments_url(@course), notice: 'SSID will start to process the assignment now. Please refresh this page after a few minutes to view the similarity results.'
-      # Don't process the file and show error if the mapping was enabled but no mapping file was uploaded
-      elsif (isMapEnabled && params[:assignment]["mapfile"].nil?)
-        @assignment.errors.add(:mapfile, "containing mapped student names need to be uploaded if the 'Upload map file' box is ticked")
-        return render action: "show"
-      # Don't process the file and show error if the mapping was enabled but no mapping file was uploaded
-      else 
-        @assignment.errors.add :mapfile, "containing mapped student names must be a valid csv file"
-        return render action: "show"
-      end
-    # Student submission file is not a valid zip file
+    
+    if (!params[:assignment].nil? && (is_valid_zip?(params[:assignment]["file"].content_type, params[:assignment]["file"].path)))
+      self.start_upload(@assignment, params[:assignment]["file"])
+      redirect_to course_assignments_url(@course), notice: 'File was successfully uploaded.'
     else
-      @assignment.errors.add :file, "containing student submission files must be a valid zip file"
-      if (params[:assignment]["mapfile"].nil? && isMapEnabled)
-        @assignment.errors.add :mapfile, "containing mapped student names need to be uploaded if the 'Upload map file' box is ticked"
-      elsif !(is_valid_map_or_no_map?(isMapEnabled, params[:assignment]["mapfile"])) 
-        @assignment.errors.add :mapfile, "containing mapped student names must be a valid csv file"
+      if params[:assignment].nil?
+          @assignment.errors.add :file, "is not selected for upload"
+      elsif params[:assignment]["file"].content_type != "application/x-zip-compressed"
+          @assignment.errors.add :file, "for upload must be a zip file"
       end
-      return render action: "show"
+      return render action: "show" 
     end
   end
 
@@ -185,14 +137,14 @@ class AssignmentsController < ApplicationController
     redirect_to course_assignments_url(@course), notice: 'Assignment was successfully deleted.'
   end
   
-  def start_upload(assignment, submissionFile, isMapEnabled, mapFile)
+  def start_upload(assignment, file)
       require 'submissions_handler'
 
       # Process upload file
-      submissions_path = SubmissionsHandler.process_upload(submissionFile, isMapEnabled, mapFile, assignment)
+      submissions_path = SubmissionsHandler.process_upload(file, assignment)
 
       # Launch java program to process submissions
-      SubmissionsHandler.process_submissions(submissions_path, assignment, isMapEnabled)
+      SubmissionsHandler.process_submissions(submissions_path, assignment)
   end
 
   # Responsible for verifying whether a uploaded file is zip by checking its mime type and/or whether can it be extracted by the zip library.
@@ -228,20 +180,6 @@ class AssignmentsController < ApplicationController
     false
   ensure
     zip.close if zip
-  end
-
-  def is_valid_map_or_no_map?(isMapEnabled, mapFile)
-    if (!isMapEnabled)
-      return true
-    else
-      if (mapFile.nil?) 
-        return false
-      elsif mapFile.content_type == "text/csv" && mapFile.path.split('.').last.to_s.downcase == 'csv'
-        return true
-      else 
-        return false
-      end
-    end
   end
 
 end
