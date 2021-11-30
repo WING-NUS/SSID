49aae83e4056e31cfbe385e697108b7e014eddac
maihuong <nguyenmaihuong9009@gmail.com>
2021-11-30 22:17:09 +0700

    update site modal to accommodate for confirm plagiarism and etc dialogs

--- app/helpers/site_modal.rb
+++ app/helpers/site_modal.rb
@@ -1,7 +1,8 @@
 class SiteModal
-  def initialize(title, supporting_text)
+  def initialize(title, supporting_text, primary_action)
     @title = title
     @supporting_text = supporting_text
+    @primary_action = primary_action
   end
-  attr_reader :title, :supporting_text
+  attr_reader :title, :supporting_text, :primary_action
 end
