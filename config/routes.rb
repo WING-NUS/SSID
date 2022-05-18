3543efa3655f93d3c810204c9ee62329de172476
maihuong <nguyenmaihuong9009@gmail.com>
2022-05-18 19:09:41 +0800

    remove a duplicate in route

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,6 @@ SSID::Application.routes.draw do
   get 'assignments/:id/submission_similarities/guest_user' => 'submission_similarities#create_guest_user', :as => 'guest_user_create'
   get 'guest_user/:id' => "sessions#check_hash"
 
-  get "cover" => "sessions#index"
   # Login/Logout routes
   get "cover" => "sessions#index"
   get "login" => "sessions#new"
