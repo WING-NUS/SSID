5de08546cddc8841738e546a8305b11864639e59
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-27 09:04:12 +0800

    Delete config directory

--- config/environment.rb
+++
@@ -1,5 +0,0 @@
-# Load the rails application
-require_relative 'application'
-
-# Initialize the rails application
-SSID::Application.initialize!
--- config/initializers/cookies_serializer.rb
+++
@@ -1,5 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Specify a serializer for the signed and encrypted cookie jars.
-# Valid options are :json, :marshal, and :hybrid.
-SSID::Application.config.action_dispatch.cookies_serializer = :json
