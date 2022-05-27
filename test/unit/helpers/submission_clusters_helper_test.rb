3ff25315a633e1713a24019b8403ca623425eeff
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-27 09:06:54 +0800

    Delete test directory

--- test/unit/helpers/submission_clusters_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SubmissionClustersHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/submission_similarity_processes_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class SubmissionSimilarityProcessesHelperTest < ActionView::TestCase
-end
