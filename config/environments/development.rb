ce32a1942b93814c96fd83503ab00265213cdd2b
maihuong <nguyenmaihuong9009@gmail.com>
2022-06-01 20:25:15 +0800

    update mailer config for development and production envs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,12 +16,13 @@ SSID::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.smtp_settings = {
-    address: "localhost",
-    port: 587
-  }
+  # mailer config
   config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.file_settings = {
+    :location => Rails.root.join('tmp/mails') 
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,10 @@ SSID::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :sendmail
+  
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
