0d87d5c72edd189c178814647b1cc6780f141e53
maihuong <nguyenmaihuong9009@gmail.com>
2022-06-10 09:53:09 +0800

    update db migration script: make user email unique

--- db/migrate/20220524041756_add_email_to_users.rb
+++ db/migrate/20220524041756_add_email_to_users.rb
@@ -1,8 +1,7 @@
 class AddEmailToUsers < ActiveRecord::Migration[6.0]
   def change
-    # TODO: Ensure new user records will have email address for password reset service
-    # add_column :users, :email, :string, { null: false }
+
     add_column :users, :email, :string
-    add_index :users, :email
+    add_index :users, :email, unique: true
   end
 end
