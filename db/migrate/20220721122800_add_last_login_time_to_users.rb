5c69baa2f1dbcdd19ad714a5b49b1e76589b5fb1
huyuxin0429 <huyuxin0428@gmail.com>
2022-07-24 16:33:28 +0800

    Add sign up page

--- db/migrate/20220721122800_add_last_login_time_to_users.rb
+++ db/migrate/20220721122800_add_last_login_time_to_users.rb
@@ -1,5 +1,5 @@
 class AddLastLoginTimeToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :last_login_time, :datetime, :null => false
+    add_column :users, :last_login_time, :datetime, :null => false, default: Time.now
   end
 end
