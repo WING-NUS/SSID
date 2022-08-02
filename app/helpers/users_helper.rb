f6a5fd0a631a408637a77d5d88f951d05306948e
huyuxin0429 <huyuxin0428@gmail.com>
2022-08-02 19:11:39 +0800

    Edited users_helper password_confirmation for consistent terminology

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,13 @@
 module UsersHelper
   private
 
-  def self.validate_password(user, password, confirm_password)
+  def self.validate_password(user, password, password_confirmation)
     user.errors.add :password, "cannot be blank" if password.empty?
     user.errors.add :password, "must be at least #{User::MIN_PASSWORD_LENGTH} characters long" if password.length < User::MIN_PASSWORD_LENGTH
     user.errors.add :password, "must contain at least 1 lower case character" if (password =~ /[a-z]+/).nil?
     user.errors.add :password, "must contain at least 1 upper case character" if (password =~ /[A-Z]+/).nil?
     user.errors.add :password, "must contain at least 1 digit or special character" if (password =~ /[0-9~!@#$%^&*()+=|]+/).nil?
-    user.errors.add :confirm_password, "does not match password" if password != confirm_password    
+    user.errors.add :password_confirmation, "does not match password" if password != password_confirmation    
   end
 
   def self.validate_email(user, email)
