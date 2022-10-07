5fa21e363e920d4cf0bd2e824a27406274cef0e3
sibinhho99 <sibinhho99@gmail.com>
2022-10-07 18:54:46 +0800

    Refactor

--- app/controllers/submission_similarities_controller.rb
+++ app/controllers/submission_similarities_controller.rb
@@ -105,10 +105,6 @@ class SubmissionSimilaritiesController < ApplicationController
   # GET /assignments/1/submission_similarities/1/view_printable
   def view_printable
     @submission_similarity = SubmissionSimilarity.find(params["submission_similarity_id"])
-    @submission1 = @submission_similarity.submission1
-    @submission2 = @submission_similarity.submission2
-    @student1 = @submission1.student
-    @student2 = @submission2.student
 
     render partial: "pair_report"
   end
