264314d7837b6fd443078c0b4cd76135d8681ba4
huyuxin0429 <e0564960@u.nus.edu>
2023-01-15 08:29:59 +0800

    Update schema for Devise integration

--- db/migrate/20221226085504_add_devise_to_users.rb
+++ db/migrate/20221226085504_add_devise_to_users.rb
@@ -25,7 +25,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.0]
       t.string   :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
