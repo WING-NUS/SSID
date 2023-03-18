79c74e02dee8cd39432166573c2ccc41bfd30717
mewrion <61722401+mygau91@users.noreply.github.com>
2023-03-19 01:15:18 +0800

    git ignore files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 SSID::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.eager_load = false
+  config.eager_load = false 
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
