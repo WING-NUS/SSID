ae8088fceb636e8b63c98a009aeb49e2729952a6
maihuong <nguyenmaihuong9009@gmail.com>
2023-06-13 15:41:30 +0800

    storing app id and etc outside of source code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -178,12 +178,12 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  config.password_length = 8..128
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  config.email_regexp = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET']
+  config.omniauth :google_oauth2, Rails.application.secrets.google_oauth_client_id, Rails.application.secrets.google_oauth_client_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
