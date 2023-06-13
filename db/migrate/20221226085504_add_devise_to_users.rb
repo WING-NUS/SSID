caa7da7ece55dc1458a665ac7be50286a2c49339
maihuong <nguyenmaihuong9009@gmail.com>
2023-06-13 15:49:48 +0800

    rename password column

--- db/migrate/20221226085504_add_devise_to_users.rb
+++ db/migrate/20221226085504_add_devise_to_users.rb
@@ -2,10 +2,11 @@
 
 class AddDeviseToUsers < ActiveRecord::Migration[6.0]
   def self.up
+    rename_column :users, :password_digest, :encrypted_password
+
     change_table :users do |t|
       ## Database authenticatable
       # t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -47,10 +48,9 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.0]
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
     # raise ActiveRecord::IrreversibleMigration
-
+    rename_column :users, :encrypted_password, :password_digest 
     change_table :users do |t|
       ## Database authenticatable
-      t.remove :encrypted_password
 
       ## Recoverable
       t.remove :reset_password_token, :reset_password_sent_at
--- db/migrate/20221226110335_remove_old_authentication_columns.rb
+++ db/migrate/20221226110335_remove_old_authentication_columns.rb
@@ -1,6 +1,5 @@
 class RemoveOldAuthenticationColumns < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :password_digest
     remove_column :users, :activation_digest
     remove_column :users, :activated
     remove_column :users, :activated_at
