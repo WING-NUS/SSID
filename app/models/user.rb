ad3ab231c2b0ba4b4d875d78f4dff14096a35fba
Hu Yuxin <e0564960@u.nus.edu>
2023-07-05 07:35:56 +0000

    Add spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,7 +83,7 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-
+ 
   def User.new_token
     SecureRandom.urlsafe_base64
   end
