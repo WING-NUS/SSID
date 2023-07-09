fcaf6b8b6de1de1c4d9a077ba489ab21ff1b04d6
Jason Christopher <jason.christopher@u.nus.edu>
2023-07-10 02:25:35 +0800

    Add anonymised files

---
+++ Tools/Anonymization/Test/Student-02.py
@@ -0,0 +1,43 @@
+# Name: Peh Li Yan
+# Matriculation: A23456789E
+# Course: CS1010
+
+# Q1
+def is_anagram(s1, s2):
+    
+    if len(s1) != len(s2):
+        return False
+    s1 = s1.lower()
+    s2 = s2.lower()
+    
+    s1List = []
+    for letter in s1:
+        s1List.append(letter)
+        
+    for letter in s2:
+        if letter in s1List:
+            s1List.remove(letter)
+            
+    if len(s1List)==0:
+        return True
+    else:
+        return False
+        
+# Q2    
+def binom_coeff(n, k):
+    x = n - k
+    return ((factorial(n))//(factorial(k)*(factorial(x))))
+    
+def factorial(y):
+    fact = 1
+    for i in range(1,y+1):
+        fact = fact*i
+    return fact
+    
+# Q3
+def binom_coeff_recur(n, k):
+    if k == 0 or n == k:
+        return 1
+    else:
+        return int(binom_coeff_recur(n-1,k-1)) + int(binom_coeff_recur(n-1,k))
+    
