d04155611d0092f8224c2df3eef53dfb0fc030bc
MENGYEWGAU_UBUNTU <gaumengyew@gmail.com>
2023-09-10 14:20:37 +0800

    Selection by a dedicated class instead of elements and removed unused logs

--- app/assets/javascripts/submission_obfuscation.js
+++ app/assets/javascripts/submission_obfuscation.js
@@ -1,48 +1,43 @@
-(function() {
+(function () {
     let SubmissionObfuscation = {};
 
     // Retrieve the "obfuscate" parameter from the URL
     let urlParams = new URLSearchParams(window.location.search);
     window.shouldNotObfuscate = (urlParams.get('obfuscate') === 'false');
 
-    // Store the original student2 name
-    SubmissionObfuscation.originalStudent2Name = null;
+    SubmissionObfuscation.maskStudentName = function () {
 
-    SubmissionObfuscation.maskStudentName = function() {
-        // console.log("JS Script here onwards");
-        // console.log(window.shouldNotObfuscate)
-    
         // Check if the global flag for obfuscation is false; if so, return without obfuscating
         if (window.shouldNotObfuscate) {
-            // console.log("Names are not obfuscated")
             return;
         }
-    
+
         // console.log("Masking student name now");
-    
-        // Masking inside all h4 elements to get the student pairings
-        let h4Elements = document.querySelectorAll('h4');
-        h4Elements.forEach(h4Element => {
+
+        // Masking inside all h4 elements with id "submission_similarities_for_students" to get the student pairings
+        let h4Element = document.querySelector('h4[class="submission_similarities_for_students"]');
+        if (h4Element) {
             let match = h4Element.innerHTML.match(/for Students .*? and (.*?) \(\d+\.\d+%\)/);
             if (match && match[1] && match[1] !== '******') {
                 let student2Name = match[1];
                 h4Element.innerHTML = h4Element.innerHTML.replace(new RegExp("and\\s" + student2Name, "g"), "and ******");
             }
-        });
-        
-        // Masking the second h5 for each pair of submissions
-        let h5Elements = document.querySelectorAll('h5');
-
-        for (let i = 1; i < h5Elements.length; i += 2) {
-            // Adjusted regex to capture student names like Student-02 or x-v6551_q1
-            h5Elements[i].innerHTML = h5Elements[i].innerHTML.replace(/Submission by [\w-]+[_\w]*/, 'Submission by ******');
         }
-        
-    
-        // Masking inside all table headers in every table
+
+        // Masking h5 elements with the class 'submission2_by_student2'
+        let h5Elements = document.querySelectorAll('h5.submission2_by_student2');
+
+        h5Elements.forEach(h5Element => {
+            // Adjusted regex to capture student names like Student-02 or x-v6551_q1
+            h5Element.innerHTML = h5Element.innerHTML.replace(/Submission by [\w-]+[_\w]*/, 'Submission by ******');
+        });
+
+
+
+        // Masking inside table header with class "student2_submission" in every table
         let tables = document.querySelectorAll('table');
         tables.forEach(table => {
-            let thirdColumnHeader = table.querySelector('th.lines_col:nth-child(3)');
+            let thirdColumnHeader = table.querySelector('th.student2_submission');
             if (thirdColumnHeader) {
                 // Adjusted regex to capture student names like Student-02 or x-v6551_q1
                 let match = thirdColumnHeader.innerHTML.match(/Submission by ([\w-]+[_\w]*)/);
@@ -51,18 +46,9 @@
                 }
             }
         });
-        
-    };
-    
 
-    /*
-    SubmissionObfuscation.onLoad = function() {
-        if (!window.shouldNotObfuscate) {
-            console.log("On Load called")
-            SubmissionObfuscation.maskStudentName();
-        }
+
     };
-    */
 
     // Expose SubmissionObfuscation to the global context
     window.SubmissionObfuscation = SubmissionObfuscation;
