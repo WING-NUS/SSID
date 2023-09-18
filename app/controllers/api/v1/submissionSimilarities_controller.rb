262156fba79d6da7ab34364737adca125642d757
Michael Gau <61722401+mengyewgau@users.noreply.github.com>
2023-09-18 12:54:58 +0800

    Update and rename submission_similarities.rb to submissionSimilarities_controller.rb

--- app/controllers/api/v1/submissionSimilarities_controller.rb
+++ app/controllers/api/v1/submissionSimilarities_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::SubmissionSimilaritiesController < ApplicationController
     skip_before_action :authenticate_user!
+    before_action :set_course_and_assignment
 
     def sample_get
         api_key = ApiKey.find_by(value: request.headers["X-API-KEY"])
         
         if api_key
-            if authorized_for_course?(api_key.user, @requested_course) 
+            if authorized_for_course?(api_key.user, @course) 
                 render json: {"Status": "OK"}, status: :ok
             else
                 render json: {"Status": "Unauthorized access"}, status: :unauthorized
@@ -19,7 +20,7 @@ class Api::V1::SubmissionSimilaritiesController < ApplicationController
         api_key = ApiKey.find_by(value: request.headers["X-API-KEY"])
         
         if api_key
-            if authorized_for_course?(api_key.user, @requested_course) 
+            if authorized_for_course?(api_key.user, @course) 
                 render json: {"Status": "OK"}, status: :ok
             else
                 render json: {"Status": "Unauthorized access"}, status: :unauthorized
@@ -31,7 +32,7 @@ class Api::V1::SubmissionSimilaritiesController < ApplicationController
 
     def get_similarities_for_assignment
         assignment_id = params[:assignment_id]
-        assignment = Assignment.find_by(id: assignment_id)
+        assignment = Assignment.find_by(id: params[:assignment_id])
     
         if assignment.nil?
             render json: {"Status": "Assignment not found"}, status: :not_found
@@ -45,8 +46,12 @@ class Api::V1::SubmissionSimilaritiesController < ApplicationController
         render json: submission_similarities
     end
     
-
     private
+
+    def set_course_and_assignment
+        @course = Course.find(params[:course_id]) if params[:course_id]
+        @assignment = Assignment.find(params[:assignment_id]) if params[:assignment_id]
+    end
 
     def authorized_for_course?(user, course)
         user_course_membership = UserCourseMembership.find_by(user: user, course: course)
