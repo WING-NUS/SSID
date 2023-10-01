8433a1219ecbed19de8a0fc3e9b9859a41c366a9
sibinhho99 <hsbinh@live.com>
2023-10-01 09:35:22 +0800

    Revert "Fix extra render when uploading invalid zip file (#342)"
    This reverts commit d6a720fe2e9cf2fc6071ee045668f83c6d97559d.

--- config/routes.rb
+++ config/routes.rb
@@ -137,12 +137,4 @@ SSID::Application.routes.draw do
       end
     end
   end
-
-  namespace :api do
-    namespace :v1 do
-      resources :courses, controller: 'courses' do
-        resources :assignments, controller: 'assignments'
-      end
-    end
-  end
 end
