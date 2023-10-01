95e81443ed3ccabb8e7f7d814411cc7420a9d244
sibinhho99 <sibinhho99@gmail.com>
2023-10-01 09:33:38 +0800

    Fix extra render when uploading invalid zip file (#342)
    * Fix extra render when uploading invalid zip file

--- config/routes.rb
+++ config/routes.rb
@@ -137,4 +137,12 @@ SSID::Application.routes.draw do
       end
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :courses, controller: 'courses' do
+        resources :assignments, controller: 'assignments'
+      end
+    end
+  end
 end
