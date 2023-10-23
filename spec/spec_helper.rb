a3fc58c5f207a3918121c577116f632e589bdcd6
huyuxin0429 <huyuxin0428@gmail.com>
2023-10-23 09:57:13 +0800

    Fix spec_helper capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,25 @@ require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-lcov'
 
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w[headless enable-features=NetworkService,NetworkServiceInProcess]
+    }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.default_driver = :headless_chrome
+Capybara.javascript_driver = :headless_chrome
+
 Capybara.configure do |config|
   config.app_host = 'http://127.0.0.1:3000/'
 end
